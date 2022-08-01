e120d8c1a0c3743f7a09abf51c10f491ddd2c850
mayankkamboj47 <mayankkamboj47@users.noreply.github.com>
2022-08-01 06:49:58 +0000

    deploy: 902528dd3ba5d7d3c31c0d1072724697bbaec934

--- .eleventy.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(eleventyConfig){
-    eleventyConfig.addFilter('humanise', function(dateString){
-        const date = new Date(dateString);
-        const month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug',
-                       'Sep', 'Oct', 'Nov', 'Dec'][date.getMonth()]
-        return `${month} ${date.getDate()}, ${date.getFullYear()}`;
-    })
-}
